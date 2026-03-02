e4ebbed66f1ea0eef7e0be0ea314f1c45a1fd119
Christina Ortiz <ortizchristina881@gmail.com>
2026-03-01 23:57:19 -0500

    final

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { useState,useEffect } from 'react'
+import  supabase  from '/supabase/supabase-js';
 
 export const Menu = () => {
-    const[menuItems,setMenuItems]= use([]);
+    const[menuItems,setMenuItems]= useState([]);
     useEffect(()=>{
-        
+    async function fetchMenu() {
+      const {data,error}= await
+
+
+      
+    }   
 
     },[]);
 
